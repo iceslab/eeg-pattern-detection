dd4e12ee5f0d34c7a8d8a6c97d19457441dca35f
Bartosz Cieśla <bartociesla@gmail.com>
2018-03-11 20:14:08 +0100

    Ploting of first read file.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import listdir
 from os.path import isfile, join
+import matplotlib.pyplot as plt
 
 DATASET_DIR = "datasets/bonn/"
 onlyfiles = [f for f in listdir(DATASET_DIR) if isfile(join(DATASET_DIR, f))]
@@ -9,6 +10,8 @@ print("Found " + str(len(onlyfiles)) + " files in directory: " + DATASET_DIR)
 content = []
 for i in range(0, len(onlyfiles)):
     with open(DATASET_DIR + onlyfiles[i], 'r') as content_file:
-        list.append(content, content_file.read())
+        list.append(content, content_file.read().splitlines())
 
 print(len(content))
+plt.plot(content[0])
+plt.show()
